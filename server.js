ecc5270b5273472ecb762978715e0a6259abb065
Eligrah Philip <suryananda7963@gmail.com>
2025-10-20 01:52:41 +0700

    Update PORT untuk Render

--- server.js
+++ server.js
@@ -503,6 +503,7 @@ app.get("/user", ensureLogged, (req, res) => {
 // ====================
 // START SERVER
 // ====================
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running at: http://localhost:${PORT}`);
 });
